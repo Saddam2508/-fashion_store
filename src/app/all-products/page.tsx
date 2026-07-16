92233a1464c8814196cd5c53bcffad6cf5f4989b
Md Saddam Hossain <eyarinsultana@gmail.com>
2026-07-16 18:07:01 +0600

    product data fetch function added

---
+++ src/app/all-products/page.tsx
@@ -0,0 +1,23 @@
+
+import { fetchProductData } from "@/helper/fetchData";
+import { Product } from "@/types/product";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "All Product | Fashion Store",
+};
+
+const AllTilesPage = async () => {
+  const result = await fetchProductData();
+
+  if (!result) return <p> No data found</p>;
+  const productData: Product[] = result;
+  console.log(productData)
+  return (
+    <div>
+      {/* <AllTiles tilesData={productData} /> */}
+    </div>
+  );
+};
+
+export default AllTilesPage;
---
+++ src/helper/fetchData.tsx
@@ -0,0 +1,13 @@
+import { Product} from "@/types/product";
+
+export const fetchProductData = async () => {
+  const baseUrl = process.env.BETTER_AUTH_URL;
+  if (!baseUrl) throw new Error("Missing APP_URL");
+
+  const res = await fetch(`${baseUrl}/product_data.json`);
+
+  if (!res.ok) return null;
+
+  const tiles: Product[] = await res.json();
+  return tiles;
+};
